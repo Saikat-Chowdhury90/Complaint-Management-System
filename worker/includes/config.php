4abd130d9d335be2b025b3d8f6ae8edb81b6f91e
pranavprakashgithub <pranavprakash4777@gmail.com>
2022-03-14 17:52:50 +0530

    Initial Commit

--- worker/includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "cms";
-$bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die("Could not connect database");
-
-?>
